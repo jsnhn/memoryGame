e1315493ba72dc7a2502613dc8d53cf29c744aaa
jsnhn <jayhh122@gmail.com>
2024-02-21 09:18:04 -0800

    added a box-container and timer/turns

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 /*----- state variables -----*/
 let gameStart = false;
 
+let board, card
 /*----- cached elements  -----*/
 const bodyEl = document.querySelector('body');
 
@@ -17,29 +18,29 @@ render()
 
 function render() {
     if (!gameStart) {
-        renderSplash()
+        // renderSplash()
     }
 };
 
-function renderSplash() {
-    bodyEl.innerHTML = `
-        <div id="splash">
-            <h1><img src="https://fontmeme.com/permalink/240220/4ae8056a49f5eb97f77bb099bc7028ea.png" alt=""></h1>
-            <div class="all-img">
-                <img class="pikachu" src="https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/220px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png">
-                <img class="jigglypuff" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png">
-                <img class="mr-mime" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png" alt="">
-                <img class="psyduck" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png" alt="">
-                <img class="jynx" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/124.png" alt="">
-                <img class="fuecoco" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/909.png" alt="">
-                <img class="snorlax" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png" alt="">
-                <img class="ponyta" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/077.png" alt="">
-            </div>
-            <button id="ply-btn"><img src="https://fontmeme.com/permalink/240220/938b7d297c1cf42e4f0bf445b23a827e.png" alt=""></button>
-        </div>
-    `
-    document.getElementById('ply-btn').addEventListener('click', handleStartClick);
-};
+// function renderSplash() {
+//     bodyEl.innerHTML = `
+//         <div id="splash">
+//             <h1><img src="https://fontmeme.com/permalink/240220/4ae8056a49f5eb97f77bb099bc7028ea.png" alt=""></h1>
+//             <div class="all-img">
+//                 <img class="pikachu" src="https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/220px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png">
+//                 <img class="jigglypuff" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png">
+//                 <img class="mr-mime" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png" alt="">
+//                 <img class="psyduck" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png" alt="">
+//                 <img class="jynx" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/124.png" alt="">
+//                 <img class="fuecoco" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/909.png" alt="">
+//                 <img class="snorlax" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png" alt="">
+//                 <img class="ponyta" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/077.png" alt="">
+//             </div>
+//             <button id="ply-btn"><img src="https://fontmeme.com/permalink/240220/938b7d297c1cf42e4f0bf445b23a827e.png" alt=""></button>
+//         </div>
+//     `
+//     document.getElementById('ply-btn').addEventListener('click', handleStartClick);
+// };
 
 
 function handleStartClick(){
