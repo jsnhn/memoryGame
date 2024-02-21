a0a3c45e1a145f390e0375bd1065ccc9e230e47b
jsnhn <jayhh122@gmail.com>
2024-02-21 13:08:53 -0800

    refactored renderSplash and handleStarClick

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 /*----- constants -----*/
+const POKEMON = [
+    {
+        name: 'pikachu',
+        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/220px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png'
+    },
+    {
+        name: 'jigglypuff',
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png'
+    },
+    {
+        name: 'mr-mime',
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png'
+    },
+    {
+        name: 'psyduck',
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png'
+    },
+    {
+        name: 'jynx',
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/124.png'
+    },
+    {
+        name: 'fuecoco',
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/909.png'
+    },
+    {
+        name: 'snorlax',
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png'
+    },
+    {
+        name: 'ponyta',
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/077.png'
+    }
+]
 
-
+// not a card for your game, can be used later create a card.
 
 /*----- state variables -----*/
 let gameStart = false;
@@ -8,44 +42,53 @@ let gameStart = false;
 let board, card
 /*----- cached elements  -----*/
 const bodyEl = document.querySelector('body');
-
+const playBtn = document.createElement('button'); //use the html
 
 /*----- event listeners -----*/
-// document.getElementById('ply-btn').addEventListener('click', handleStartClick)
+playBtn.addEventListener('click', handleStartClick)
 
 /*----- functions -----*/
 render()
 
 function render() {
     if (!gameStart) {
-        // renderSplash()
+        renderSplash()
+    } else {
+        renderMain()
     }
 };
 
-// function renderSplash() {
-//     bodyEl.innerHTML = `
-//         <div id="splash">
-//             <h1><img src="https://fontmeme.com/permalink/240220/4ae8056a49f5eb97f77bb099bc7028ea.png" alt=""></h1>
-//             <div class="all-img">
-//                 <img class="pikachu" src="https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp/220px-Pikachu_artwork_for_Pok%C3%A9mon_Red_and_Blue.webp.png">
-//                 <img class="jigglypuff" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png">
-//                 <img class="mr-mime" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png" alt="">
-//                 <img class="psyduck" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png" alt="">
-//                 <img class="jynx" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/124.png" alt="">
-//                 <img class="fuecoco" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/909.png" alt="">
-//                 <img class="snorlax" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png" alt="">
-//                 <img class="ponyta" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/077.png" alt="">
-//             </div>
-//             <button id="ply-btn"><img src="https://fontmeme.com/permalink/240220/938b7d297c1cf42e4f0bf445b23a827e.png" alt=""></button>
-//         </div>
-//     `
-//     document.getElementById('ply-btn').addEventListener('click', handleStartClick);
-// };
+function renderSplash() {
+    const splashPage = document.createElement('div')
+    splashPage.id = 'splash'//changing the id
+    
+    const header = document.createElement('h1')
+    header.innerHTML = '<img src="https://fontmeme.com/permalink/240220/4ae8056a49f5eb97f77bb099bc7028ea.png" alt="">'
+
+    const allImgEl = document.createElement('div')
+    allImgEl.className = 'all-img'
+    POKEMON.forEach(function(poke){
+        const imgEl = document.createElement('img')
+        imgEl.className = poke.name
+        imgEl.src = poke.img
+        allImgEl.append(imgEl)
+    })
+    
+    playBtn.id = 'ply-btn'
+    playBtn.innerHTML = '<img src="https://fontmeme.com/permalink/240220/938b7d297c1cf42e4f0bf445b23a827e.png" alt="">'
+
+    splashPage.append(header, allImgEl, playBtn)
+    console.log(splashPage)
+
+    bodyEl.innerHTML = ''
+    bodyEl.append(splashPage)
+};
 
 
 function handleStartClick(){
-    renderMain()
-};
+    gameStart = true;
+    render()
+};// u need to update all impacted state, and just call the render button
 
 function renderMain() {
     bodyEl.innerHTML = `
